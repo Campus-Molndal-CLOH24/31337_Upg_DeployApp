cdf15dabf628890931243086feb6b2baa20f9608
sirhaffy <haffy@icloud.com>
2025-03-11 14:25:25 +0100

    Added steps in workflow

---
+++ README.md
@@ -0,0 +1,9 @@
+Grupparbete - 31337
+Medverkande medlemmar: Fredrik, Niklas, Megan, Dennis.
+
+Uppgiftens instruktioner finns här: https://cloud-developer.educ8.se/clo/2.-basic-cloud-applications/4.-tutorials/8.-use-github-actions-to-deploy-your-app/index.html
+
+# Anteckningar
++ Bytte ut .Net 8.0 till 9.0 via backports.
++ La också till så skriptet (provisioning) skriver ut VMens IP-adress.
++ 
---
+++ infrastructure/provision_vm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+resource_group=GithubActionsDemoRG
+vm_name=GithubActionsDemoVM
+vm_port=5000
+
+az group create --location northeurope --name $resource_group
+
+az vm create --name $vm_name --resource-group $resource_group \
+             --image Ubuntu2204 --size Standard_B1s \
+             --generate-ssh-keys --admin-username azureuser \
+             --custom-data @cloud-init_dotnet.yaml
+
+az vm open-port --port $vm_port --resource-group $resource_group --name $vm_name
+
+# Print the VM IP address
+vm_ip=$(az vm show --show-details --name $vm_name --resource-group $resource_group --query publicIps --output tsv)
+echo "The VM IP address is: $vm_ip"
